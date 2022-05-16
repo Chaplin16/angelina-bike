908a2f4646917fd0168ca98b69a5da9bd42152ea
Chaplin16 <chaplin16@orange.fr>
2022-05-16 18:55:35 +0200

    optimisation photos

--- js/velo.js
+++ js/velo.js
@@ -20,19 +20,9 @@ const velo = [
         alt:""
     },
     {
-        id:4,
-        link:"images/photos/location-velo-grande-motte-angelina-bike.jpeg",
-        alt:"location de vélo électriques, trottinette, draisiennes, moto pour enfants à la Grande Motte"
-    },
-    {
         id:8,
         link:"images/photos/moto-cross-piwi-enfant-location-grande-motte.jpeg",
         alt:"location moto enfant à la grande motte chez angelina bike"
-    },
-    {
-        id:5,
-        link:"images/photos/location-velo-grande-motte-achat-vente.jpeg",
-        alt:"location achat vente de vélo à la Grande Motte chez Angélina Bike"
     },
     {
         id:9,
@@ -43,11 +33,6 @@ const velo = [
         id:6,
         link:"images/photos/location-velo-grande-motte-scooter-trottinettes-magasin.jpeg",
         alt:"magasin de vélo à la grande motte, location, achat, vente "
-    },
-    {
-        id:7,
-        link:"images/photos/location-velo-grande-motte-trottinettes-magasin.jpeg",
-        alt:"location à la grande motte de vélo pour toute la famille, moto et draisienne pour enfants"
     },
     {
         id:11,
